c1f2e74022bacfc08cd1a5258cc7daf5f5aca206
Celestinno <celestino_gabriel@hotmail.com>
2022-05-25 00:07:58 -0300

    Basic functionality all done

--- app.js
+++ app.js
@@ -19,10 +19,28 @@ btn.addEventListener('click',() =>{
   .then(response => { 
     display.innerHTML = "";
     let panel = document.createElement('div');
-    display.appendChild(panel);    
+    let temp = document.createElement('div');
+    let icon = document.createElement('img');    
+    display.appendChild(panel);  
+    display.appendChild(icon);  
+    display.appendChild(temp);
     panel.innerHTML ="<p>" + response.location.name + "</p>";
     panel.classList.add('weather-panel');
-
+    icon.classList.add('weather-icon');
+    temp.classList.add("weather-temp");
+    if(response.current.precip_mm >= 0.5){
+      icon.src = 'https://cdn-icons-png.flaticon.com/512/116/116251.png';
+      icon.style="  width: 200px; height: 200px;"
+    }
+    else if (response.current.cloud > 25 && response.current.precip_mm <0.5){
+      icon.src = 'https://uxwing.com/wp-content/themes/uxwing/download/27-weather/cloudy.png';
+      icon.style="  width: 200px; height: 150px;"
+    }
+    else{
+      icon.src = 'https://www.iconpacks.net/icons/2/free-sun-icon-3337-thumb.png';
+      icon.style="  width: 200px; height: 200px;"
+    }
+    temp.innerHTML = "<p>"+ response.current.temp_c + "°C </p>";
   })
   .catch(err => console.error(err));
 } )
