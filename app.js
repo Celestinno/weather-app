91ec9b4a0479aa1169d93aa4bd47d799f47268f5
Celestinno <celestino_gabriel@hotmail.com>
2022-05-25 21:08:47 -0300

    refactored code a bit to use functions

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const btn = document.querySelector('[data-weather="submit"]')
 const input = document.querySelector('[data-weather="text"]')
 const display = document.querySelector('[data-weather="display"]')
+const rainThreshold = 0.5;
+const moderateRainThreshold = 4;
+const heavyRainThreshold = 8;
+const sunnyThreshold = 25;
+let panel;
+let temp;
+let icon;
+let summary;
+
 
 const options = {
 	method: 'GET',
@@ -10,50 +19,71 @@ const options = {
 	}
 };
 
+function getInputValue(){
+  return input.value;
+}
+function cleanElements(){
+  return "";
+}
+function createPageElements(){
+  //creates elements
+  panel = document.createElement('div');
+  temp = document.createElement('div');
+  icon = document.createElement("div");  
+  summary = document.createElement('div'); 
+  //appends them to parent div
+  display.appendChild(panel);  
+  display.appendChild(icon);  
+  display.appendChild(temp);
+  display.appendChild(summary);
+  //adds styling
+  panel.classList.add('weather-panel');
+  icon.classList.add('weather-icon');
+  temp.classList.add("weather-temp");
+  summary.classList.add("weather-summary");
+  return;
+}
 
-btn.addEventListener('click',() =>{
-  
-  let cityName = input.value;
-  fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + input.value, options)
-  .then(response => response.json())
-  .then(response => { 
-    display.innerHTML = "";
-    let panel = document.createElement('div');
-    let temp = document.createElement('div');
-    let icon = document.createElement("div");  
-    let summary = document.createElement('div');  
-    display.appendChild(panel);  
-    display.appendChild(icon);  
-    display.appendChild(temp);
-    display.appendChild(summary);
-    panel.innerHTML ="<p>" + response.location.name + "</p>";
-    panel.classList.add('weather-panel');
-    icon.classList.add('weather-icon');
-    temp.classList.add("weather-temp");
-    summary.classList.add("weather-summary");
-    if(response.current.precip_mm >= 0.5){
-      icon.innerHTML = "<img src='./images/rain.png'>";
-      if(response.current.precip_mm>=0.5 && response.current.precip_mm<4){
-        summary.innerHTML = "<p>Light Rain</p>"
-      }
-      else if (response.current.precip_mm>=4 && response.current.precip_mm<8){
-        summary.innerHTML = "<p>Moderate Rain</p>"
-      }
-      else{
-        summary.innerHTML = "<p>Heavy Rain</p>"
-      }
-      
+function addContent(response){
+  panel.innerHTML ="<p>" + response.location.name + "</p>";
+  if(response.current.precip_mm >= rainThreshold){
+    icon.innerHTML = "<img src='./images/rain.png'>";
+    if(response.current.precip_mm>= rainThreshold && response.current.precip_mm < moderateRainThreshold){
+      summary.innerHTML = "<p>Light Rain</p>"
     }
-    else if (response.current.cloud > 25 && response.current.precip_mm <0.5){
-      icon.innerHTML = "<img src='./images/cloud.png'>";
-      summary.innerHTML = "<p>cloudy</p>"
+    else if (response.current.precip_mm>= moderateRainThreshold && response.current.precip_mm < heavyRainThreshold){
+      summary.innerHTML = "<p>Moderate Rain</p>"
     }
     else{
-      icon.innerHTML = "<img src='./images/sun.png'>";
-      summary.innerHTML = "<p>Sunny</p>"
+      summary.innerHTML = "<p>Heavy Rain</p>"
     }
-    temp.innerHTML = "<p>"+ response.current.temp_c + "°C </p>";
+    
+  }
+  else if (response.current.cloud > sunnyThreshold && response.current.precip_mm < rainThreshold){
+    icon.innerHTML = "<img src='./images/cloud.png'>";
+    summary.innerHTML = "<p>cloudy</p>"
+  }
+  else{
+    icon.innerHTML = "<img src='./images/sun.png'>";
+    summary.innerHTML = "<p>Sunny</p>"
+  }
+  temp.innerHTML = "<p>"+ response.current.temp_c + "°C </p>";
+}
+
+btn.addEventListener('click',() =>{
+
+  //fetches the data from the API
+  fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + getInputValue(), options)
+  .then(response => response.json())
+  .then(response => { 
+    //cleans the HTML of the display Element
+    display.innerHTML = cleanElements();
+    //creates the divs
+    createPageElements();
+    addContent(response);
+    //sets up the top div with the name of the city
   })
+  //catches errors in the request
   .catch(err => console.error(err));
 } )
 
